5cc30e4cd158174fc8a1192b3d6bd6e4789dfd25
Dustin Kröger <me@staubrein.com>
2021-07-30 05:32:07 +0200

    add status log

--- bot.js
+++ bot.js
@@ -4,11 +4,21 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const poll = require('./poll')
+const mongo = require('./mongo')
 //const welcome = require('./welcome')
 
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`)
+    await mongo().then(mongoose => {
+        try {
+            console.log(`Successfully connected to mongodb`)
+        } finally {
+            mongoose.connection.close()
+        }
+    })
+
+    console.log('\n==== Loading commands ====')
 
     const cmdBaseFile = 'command.js';
     const cmdBase = require(`./commands/${cmdBaseFile}`);
@@ -29,10 +39,13 @@ client.on('ready', async () => {
     }
 
     readCommands('commands')
+
+    console.log('==== Commands loaded ====\n')
     cmdBase.listen(client)
 
     poll(client);
     //welcome(client);
+    console.log(`ready.`)
 });
 
 client.login(process.env.TOKEN);
