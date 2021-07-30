986ec1c6d6247f96680e63a646dc8446cca470ad
Dustin Kröger <me@staubrein.com>
2021-07-30 15:56:14 +0200

    add expected args to otty command

--- commands/otty.js
+++ commands/otty.js
@@ -4,6 +4,7 @@ module.exports = {
     minArgs: 1,
     maxArgs: 1,
     requiredRoles: ['Otty'],
+    expectedArgs: '<usertag>',
     callback: (message, arguments, text) => {
 
         const user = message.mentions.users.first();
