06f6318cd450fe85999b0b38ef3bfb6e39128961
Dustin Kröger <me@staubrein.com>
2021-08-06 02:54:17 +0200

    introduce modloader

--- util/mods.js
+++ util/mods.js
@@ -1,25 +1,52 @@
-const { prefix } = require('@root/settings.json');
 const path = require('path');
 const fs = require('fs');
 
-module.exports.load = (client) => {
-    console.log('\n==== Loading Mods ====')
+/**
+ *
+ */
+class ModLoader {
 
-    // recursively read directory for Mods
-    const readMods = dir => {
-      const files = fs.readdirSync(path.join(__dirname, dir))
-      for(const file of files) {
-          const stat = fs.lstatSync(path.join(__dirname, dir, file))
-          if (stat.isDirectory()) {
-              readMods(path.join(dir, file))
-          } else {
-              const mod = require(path.join(__dirname, dir, file))
-              console.log(`Enabling mod ${file}`)
-              mod(client)
-              //this.register(client)
-          }
-      }
+    /**
+     *
+     * @param Client client
+     * @param string dir
+     */
+    constructor(client, dir) {
+
+        this.client = client;
+        this.dir = dir;
+
+        this.load()
     }
-    readMods('../mods')
-    console.log('==== Mods loaded ====\n')
-  }
+
+    /**
+     *
+     * @param string dir
+     */
+    _load(dir) {
+        let count = 0
+        const files = fs.readdirSync(path.join(__dirname, dir))
+        for(const file of files) {
+            const stat = fs.lstatSync(path.join(__dirname, dir, file))
+            if (stat.isDirectory()) {
+                count += this._load(path.join(dir, file))
+            } else {
+                const mod = require(path.join(__dirname, dir, file))
+                console.log(`Enabling mod ${file}`)
+                mod(this.client)
+                ++count
+            }
+        }
+        return count
+    }
+
+    /**
+     *
+     */
+    load() {
+
+        this._load(this.dir)
+    }
+}
+
+module.exports.ModLoader = ModLoader
