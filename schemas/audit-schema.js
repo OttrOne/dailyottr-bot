c5a34d15ab14c2d26a47c647235da460efbbcd50
Dustin Kröger <me@staubrein.com>
2021-08-19 22:45:20 +0200

    cleanup

--- schemas/audit-schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const { reqString } = require('@schemas/types')
-
-const auditSchema = mongoose.Schema({
-    _id: reqString,
-    channelId: reqString,
-})
-
-module.exports = mongoose.model('audit-channels', auditSchema);
--- schemas/w2g-schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const { reqString } = require('@schemas/types')
-
-const w2gSchema = mongoose.Schema({
-    guildId: reqString,
-    url: reqString,
-})
-
-module.exports = mongoose.model('w2g-urls', w2gSchema);
