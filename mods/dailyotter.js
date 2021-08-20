59ad0e0412dc9d78e82c7608ff5817e0bcbc54fd
Dustin Kröger <me@staubrein.com>
2021-08-20 12:20:34 +0200

    major cleanup...less unnecessary data

--- mods/dailyotter.js
+++ mods/dailyotter.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js')
 const Parser = require('rss-parser')
 
 const dailyotterSchema = require('@schemas/dailyotter-schema')
+const configSchema = require('@schemas/doconf-schema');
 const logger = require('@util/logger')
 const { Task, addTask} = require('@util/scheduler')
 
@@ -13,98 +14,99 @@ const fetchOtters = async () => {
 
     logger.info(`[DailyOtterMod] Fetch otters`)
 
+    // parse dailyotter blog for pictures
     let feed = await parser.parseURL('https://dailyotter.org/?format=rss')
     const picRe = /data-image=\"([a-zA-Z0-9\:\.\%\-\+\/]+)\"/
 
     for (item of feed.items) {
 
+        // check for url in content
         let url = picRe.exec(item.content)
         if (url && url[1]) {
+            // check if existant.
             const exists = await dailyotterSchema.find({
                 guid: item.guid
-            })
+            }) // TODO findOneAndUpdate?
 
-            if(exists.length !== 0) return [];
+            if(exists.length !== 0) continue;
             const otter = {
-                guid: item.guid,
                 title: item.title,
                 date: item.isoDate,
                 reference: item.contentSnippet,
                 link: item.link,
                 imageUrl: url[1],
-                sent: false
             }
-            const otterSave = dailyotterSchema({
-                guid: item.guid,
-                title: item.title,
-                date: item.isoDate,
-                reference: item.contentSnippet,
-                link: item.link,
-                imageUrl: url[1],
-                sent: false
-            });
-            await otterSave.save(function (err, doc) {
+            await dailyotterSchema({
+                guid: item.guid
+            }).save(function (err, doc) {
                 if(err) {
                     logger.error(err);
                     console.log(doc)
                 }
             });
+            // push to list of new otts
             otters.push(otter)
         }
     }
-    return otters;
+    // pics from rss are ordered chronologically with newest, first.
+    // we want to send it so that the newest one is the last message
+    return otters.reverse();
 }
 const sendOtter = async (guild, otters) => {
-    const channelId = '869974559762284544'
-    const channel = guild.channels.cache.get(channelId)
-
-    logger.info(`[DailyOtterMod] Send otters to guild ${guild.name}`)
-
-    if(!channel) {
-        logger.error(`[DailyOtterMod] Channel not found on guild ${guild.name}`)
-        return;
-    }
 
     const me = guild.me.user
 
-    for (const otter of otters) {
-        const otterEmbed = new MessageEmbed()
-        .setAuthor(me.username, me.avatarURL())
-        .setTitle(otter.title)
-        .setImage(otter.imageUrl)
-        .setTimestamp(otter.date)
-        .setURL(otter.link)
-        .setFooter(`Fetched from dailyotter.org - ${otter.reference}`)
-        channel.send(otterEmbed)
+    try{
+        // get channel from mongodb
+        const channelId = (await configSchema.findOne({ _id: guild.id })).channelId
+        const channel = guild.channels.cache.get(channelId)
+        if(!channel) throw Error(`[DailyOtterMod] Channel not found on guild ${guild.name}`);
+
+        for (const otter of otters) {
+            const otterEmbed = new MessageEmbed()
+            .setAuthor(me.username, me.avatarURL())
+            .setTitle(otter.title)
+            .setImage(otter.imageUrl)
+            .setTimestamp(otter.date)
+            .setColor('#F2B749')
+            .setURL(otter.link)
+            .setFooter(`Fetched from dailyotter.org - ${otter.reference}`)
+            try {
+                await channel.send(otterEmbed)
+                logger.debug(`[DailyOtterMod] Sent Ott to Server ${guild.name}`)
+            } catch (error) {
+                logger.error(`[DailyOtterMod] Unable to send Messages in the configured channel on Server ${guild.name}`)
+                console.log(error)
+            }
+        }
+
+    } catch(err) {
+        logger.error(err)
+        return;
     }
 }
 
-const setToSent = async (otters) => {
 
-    for(otter in otters) {
-        await dailyotterSchema.updateOne({
-            guid: otter.guid
-        }, { sent: true })
-    }
-}
 
 module.exports = async (client) => {
 
-    //fetchOtters()
+    // first execution on startup
+    const otters = await fetchOtters()
+    logger.debug(`[DailyOtterMod] Found ${otters.length} new Otters !`)
+    client.guilds.cache.forEach((guild) => {
+        sendOtter(guild, otters)
+    })
 
     const updateOtters = new Task(`updateOtters`, async (context) => {
 
         const client = context[0];
-        //await fetchOtters()
+
         const otters = await fetchOtters()
-        console.log(otters)
         logger.debug(`[DailyOtterMod] Found ${otters.length} new Otters !`)
         client.guilds.cache.forEach((guild) => {
             sendOtter(guild, otters)
         })
-
-        setToSent(otters)
-    }, 10*1000, undefined, client)
+    }, 5*60*60*1000, undefined, client)
     addTask(updateOtters)
     logger.info(`[DailyOtterMod] Add Task to update otters`)
 }
--- schemas/dailyotter-schema.js
+++ schemas/dailyotter-schema.js
@@ -3,16 +3,7 @@ const { reqString, reqBoolean } = require('@schemas/types');
 
 const dailyotterSchema = mongoose.Schema({
 
-    guid: reqString,
-    title: reqString,
-    link: reqString,
-    date: {
-        type: Date,
-        required: true
-    },
-    imageUrl: reqString,
-    reference: reqString,
-    sent: reqBoolean
+    guid: reqString
 })
 
 module.exports = mongoose.model('dailyotter-pics', dailyotterSchema);
