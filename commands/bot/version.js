d64348aad645c2348d430319fc3fbead64e82d34
Dustin Kröger <me@staubrein.com>
2021-08-20 12:19:55 +0200

    add category to version command

--- commands/bot/version.js
+++ commands/bot/version.js
@@ -4,6 +4,7 @@ module.exports = { version }
 
 module.exports = {
     name: 'version',
+    category: 'LexBot',
     maxArgs: 0,
     callback: (message, arguments, text) => {
         message.channel.send(version)
